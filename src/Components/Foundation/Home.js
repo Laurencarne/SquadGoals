3ad1652a375cad480b62c15a9937006342fdda56
Lauren Carne <oreocookie@hotmail.co.uk>
2019-08-04 13:33:23 +0100

    Moving home page around

--- src/Components/Foundation/Home.js
+++ src/Components/Foundation/Home.js
@@ -5,6 +5,23 @@ class Home extends Component {
   render() {
     return (
       <div className="landingPageContainer">
+        <div className="landingPageBottom">
+          <h1> Squad Goals </h1>
+          <h3>Welcome to Squad Goals!</h3>
+          <h3>
+            This is an app to keep your flat details in one place. Log when your
+            bills are due and details about your place.
+          </h3>
+          <h3>
+            You can create of join a flat with your flatmates and keep track of
+            your communal shopping list, household events and weekly tasks.
+          </h3>
+          <h3>
+            Sign up and start creating your own flat and invite your house mates
+            to join you or take a look around a premade user:
+          </h3>
+          <h3>Username: lauren Password: 12345</h3>
+        </div>
         <div>
           <iframe
             className="video"
@@ -17,24 +34,6 @@ class Home extends Component {
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
           ></iframe>
-        </div>
-        <div className="landingPageBottom">
-          <h1> Squad Goals </h1>
-          <h3>What is it?</h3>
-          <p>Welcome to Squad Goals!</p>
-          <p>
-            This is an app to keep your flat details in one place. Log when your
-            bills are due and details about your place.
-          </p>
-          <p>
-            You can create of join a flat with your flatmates and keep track of
-            your communal shopping list, household events and weekly tasks.
-          </p>
-          <p>
-            Sign up and start creating your own flat and invite your house mates
-            to join you or take a look around a premade user:
-          </p>
-          <p>Username: lauren Password: 12345</p>
         </div>
       </div>
     );
